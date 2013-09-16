83c6dc4abbdcb1df5f095971c399500e10b29073
Luis Cordova <cordoval@gmail.com>
2013-09-16 11:51:29 -0500

    learning from guilherme

--- src/Khepin/Medusa/Command/AddRepoCommand.php
+++ src/Khepin/Medusa/Command/AddRepoCommand.php
@@ -166,10 +166,8 @@ EOT
         }
 
         foreach ($this->config->repositories as $repo) {
-            if (property_exists($repo, 'name')) {
-                if ($repo->name === $package) {
-                    return $repo->url;
-                }
+            if (property_exists($repo, 'name') && $repo->name === $package) {
+                return $repo->url;
             }
         }
 
